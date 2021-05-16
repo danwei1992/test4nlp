368b97dc09b48e42123f98864a40118f9a6fbd4e
unknown <1289469770@qq.com>
2021-05-16 18:10:17 +0800

    'update'

--- chatbot_seq2seq/predict/chatbot_seq2seq_predict.py
+++ chatbot_seq2seq/predict/chatbot_seq2seq_predict.py
@@ -1,7 +1,13 @@
 #! -*- coding: utf-8 -*-
 from test4nlp.chatbot_seq2seq.train.chatbot_seq2seq_train import model, tokenizer, chatbot
+import json
 
 def chatbot_seq2seq(dialogue):
     response = chatbot.response(dialogue)
     return response
 
+if __name__ == "__main__":
+    dialogue = input("请输入对话：")
+    dialogue = json.loads(dialogue)
+    result = chatbot_seq2seq(dialogue)
+    print(result)
